5b40c7c8316f269f96b3813ccf1b57dec23f11d4
Thomas Dudziak <thomas@ning.com>
2012-04-29 16:51:14 -0700

    Inital checkin

---
+++ Gemfile
@@ -0,0 +1,15 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.3'
+gem 'activerecord-jdbcsqlite3-adapter'
+gem 'jruby-openssl'
+gem 'json'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'therubyrhino'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JrubyRailsJettySkeleton::Application.config.secret_token = '20cd07611046a5a1f6497dd907972878a62a0aa4037f2a15be704e7d03809e25ad90664ffb05db8004fa610c961cfc9f526f2995a63cadb6371bdc6eddf5acc8'
